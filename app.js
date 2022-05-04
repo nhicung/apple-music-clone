ac768eb34ec168c06ac4ce5abcf69a03c3bd02e6
Nhi Cung <cunghonglinhnhi@gmail.com>
2022-05-04 10:41:36 -0700

    Updatte the app.js to serve for react app

--- app.js
+++ app.js
@@ -5,25 +5,22 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config({ path: __dirname + "/.env" });
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 var spotifyRouter = require("./routes/spotify");
 
 var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static files from react app
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/spotify", spotifyRouter);
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
